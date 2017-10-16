252a45777a5a9ea59e88a0609b8c71c70f6c3ea0
Peter Wu <pwu@cloudflare.com>
2017-10-16 16:48:14 +0100

    Set deadline to 5s
    Hopefully this is high enough even for the slowest networks.

--- main.go
+++ main.go
@@ -247,6 +247,7 @@ func DialTCP(network, address string) (*Conn, error) {
 		socketId: socketId,
 		ioResult: make(chan error, 1),
 	}
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
 	registerSocket(conn)
 	if err = conn.connect(host, port); err != nil {
 		socketCall("destroy", socketId)
