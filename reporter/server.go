06b1d3cacf3f1702dbc352aa4ba213251218f9e6
Peter Wu <pwu@cloudflare.com>
2017-12-08 16:04:59 +0000

    reporter: implement host router and add debugging

--- reporter/server.go
+++ reporter/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -18,6 +21,39 @@ const (
 	hostSuffixIPv6 = ".l6.ls-l.info" // IPv6 tests
 )
 
+func getCertificate(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	// TODO select certificate based on host
+	cert, err := tls.LoadX509KeyPair("server.pem", "server.pem")
+	return &cert, err
+}
+
+type hostHandler struct {
+	http.Handler
+	reporterHandler http.Handler
+}
+
+func (h *hostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Strip port number from host
+	host, _, _ := net.SplitHostPort(r.Host)
+	if host == "" {
+		host = r.Host
+	}
+	host = strings.ToLower(host)
+
+	if host == hostReporter {
+		// TODO implement reporter handler
+		//h.reporterHandler.ServeHTTP(w, r)
+		return
+	}
+	if strings.HasSuffix(host, hostSuffixIPv4) || strings.HasSuffix(host, hostSuffixIPv6) {
+		// magic response that should be checked for by the client
+		w.Write([]byte("Hello world!\n"))
+		return
+	}
+
+	http.Error(w, "site is not configured", http.StatusNotFound)
+}
+
 func main() {
 	address := ":4433"
 	l, err := net.Listen("tcp", address)
@@ -25,7 +61,29 @@ func main() {
 		panic(err)
 	}
 	wl := newListener(l)
-	err = http.ServeTLS(wl, nil, "server.pem", "server.pem")
+
+	hostRouter := &hostHandler{}
+
+	tlsConfig := &tls.Config{
+		GetCertificate: getCertificate,
+	}
+
+	if keylogFilename := os.Getenv("SSLKEYLOGFILE"); keylogFilename != "" {
+		kw, err := os.OpenFile(keylogFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
+		if err != nil {
+			log.Printf("failed to write key log file: %v\n", err)
+		} else {
+			log.Printf("Enabled keylogging to %v\n", keylogFilename)
+			defer kw.Close()
+			tlsConfig.KeyLogWriter = kw
+		}
+	}
+
+	server := &http.Server{
+		Handler:   hostRouter,
+		TLSConfig: tlsConfig,
+	}
+	err = server.ServeTLS(wl, "", "")
 	if err != nil {
 		log.Printf("ServeTLS failed: %v\n", err)
 	}
